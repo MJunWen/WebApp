08655bb31bcd7583f38df1012f32430fe44a4328
MJunWen <mokjunwen@gmail.com>
2020-11-22 21:52:55 +0800

    regex

---
+++ 1516/1c.js
@@ -0,0 +1,17 @@
+// 1c) Express the regular expressions using Javascript core object RegExp() to validate the following:
+// 1ci) Student's name - It is  a string of individual character words seperated by character spaces between them. The character words
+// may include hypen ('-') but not any numeric digit. The first and last characters of the string should be an alphabet character
+var regexp1ci = new RegExp ('^[A-Za-z]+([\s]?[A-Za-z\-])*[A-Za-z]+'); //(Any alphabet) 1 or more times , (0 or 1 space ,any alphabet or - ) 0 or more times, (Any alphabet) 1 or more times
+
+// 1cii)Matriculation number string of 7 numeric digits. The string is preceded by two or three characters and ends with a single character.
+// There should be no white space within the entire sring
+var regexp1cii = new RegExp ('');
+
+
+var regexp1ciii = new RegExp ('');
+
+
+var regexp1civ = new RegExp ('');
+
+
+var regexp1cv = new RegExp ('');
