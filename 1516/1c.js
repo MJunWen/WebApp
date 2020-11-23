7c3a030bdb9d2bded7aeb6cf586cfdb596c51f90
MJunWen <mokjunwen@gmail.com>
2020-11-23 12:40:59 +0800

    done

--- 1516/1c.js
+++ 1516/1c.js
@@ -11,10 +11,16 @@ var regexp1cii = new RegExp (/^[A-Za-z]{2,3}\d{7}[A-za-z]{1}$/);
 //and maybe contain . or - . The domain name may have 1 or more address extensions, each seperated by . . Last extension 2-4 characters
 
 
-var regexp1ciii = new RegExp (/^[\w.-]+@[\w.-]*[.][\w]{2,4}$/);
+var regexp1ciii = new RegExp (/^[\w.-]+@[\w.-]+[.][\w]{2,4}$/); // matches a word, . or - one or more times || @ || word, . - one or more times
+//must match a . , then a word with 2-4 characters to signify its the end
+
+//1civ) Phone number. 8 numeral digits, begin with 6 8 or 9 . May start with +(65)
 
 
-var regexp1civ = new RegExp (/a/);
+var regexp1civ = new RegExp (/^(\+\(65\))?[689]\d{7}/); //[] means match 1 item inside the bracket.
+//Canot do [+(65)] because that means match only 1 item inside the square brackets i.e only +, (, 6 , 5 , )
+//can do ([+][(]65[)])? which is replace \ with [] but looks confusing af
 
-
-var regexp1cv = new RegExp (/a/);
+//1cv)
+var phonenumber = document.getElementById("phnumber");
+recexp1civ.test(phonenumber);
