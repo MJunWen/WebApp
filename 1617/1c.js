510df1ad19e0d7dcd3867ad48b1c2cf8225484e1
MJunWen <mokjunwen@gmail.com>
2020-11-23 21:39:16 +0800

    done

---
+++ 1617/1c.js
@@ -0,0 +1,11 @@
+//1ci
+var regexp1ci = /^[A-Za-z]*[\s\.]?[A-Za-z]+$/;
+
+//1cii
+var regexp1cii = /^[A-Z]\d{7}[A-Z]$/;
+
+//1ciii
+var regexp1cii = /^\d{4}[-]?\d{4}$/;
+
+//1civ
+var regexp1cii = //;
